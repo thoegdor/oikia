c51a61f14f711802f43efae7b54e52958f9e48dd
thoegdor <thoegodor@outlook.com>
2019-09-04 14:48:41 -0400

    start controller

---
+++ control.py
@@ -0,0 +1,5 @@
+import requests
+from pprint import pprint
+import datetime
+from helper import D
+from weather import weather_reporter
--- weather.py
+++ weather.py
@@ -3,42 +3,42 @@ from pprint import pprint
 import datetime
 from helper import D
 
+def weather_reporter(D):
 
-API_key = D['weather_API_key']
-base_url = "https://api.openweathermap.org/data/2.5/forecast?"
+    API_key = D['weather_API_key']
+    base_url = "https://api.openweathermap.org/data/2.5/forecast?"
+    city_zip = D['weather_zip']
+    final_url = base_url + "appid=" + API_key + "&zip=" + city_zip + ",us"
+    weather_data = requests.get(final_url).json()
 
-city_zip = D['weather_zip']
- 
-final_url = base_url + "appid=" + API_key + "&zip=" + city_zip + ",us"
-
-weather_data = requests.get(final_url).json()
-
-# need to parse that bad boy
-# first eight entries correspond to current date
-# just going to make a decent ascii table for printing to command line for now
-
-# cols: time, humidity, temp(f), desc
-
-# need a kelvin converter
-
-def kelvin_to_fahr(temp_in_k):
-    f = ((temp_in_k - 273.15) * 9)/5 + 32
-    return round(f,2)
-
-weather_list = weather_data['list']
-container_list = [['datetime','humidity','temp(f)','desc']]
-
-for h in range(0,9):
-    # get vals, then stuff
-    dtime = weather_list[h]['dt_txt']
-    humidity = weather_list[h]['main']['humidity']
-    temp = kelvin_to_fahr(weather_list[h]['main']['temp'])
-    desc = weather_list[h]['weather'][0]['description']
-    row = [dtime,humidity,temp,desc]
-    container_list.append(row)
+    # need to parse that bad boy
+    # print cols: time, humidity, temp(f), desc + emoji
     
-current_date = datetime.datetime.now().strftime("%Y-%m-%d")
+    def kelvin_to_fahr(temp_in_k):
+        f = ((temp_in_k - 273.15) * 9)/5 + 32
+        return round(f,2)
+    
+    #desc_emoji_dict = {
+    #    "moderate rain":u'\U0001F327',
+    #    "light rain":u'\U0001F327',
+    #    "clear sky":u'\U0001F31D',
+    #    "broken clouds":u'U0001F324',
+    #    "overcast clouds":u'U0001F325'
+    #}
 
-pprint(container_list)
+    weather_list = weather_data['list']
+    container_list = [['datetime','humidity','temp(f)','desc']]
 
+    for h in range(0,9): # 24 hours of entries
+        # get vals, then stuff
+        dtime = weather_list[h]['dt_txt']
+        humidity = weather_list[h]['main']['humidity']
+        temp = kelvin_to_fahr(weather_list[h]['main']['temp'])
+        desc = weather_list[h]['weather'][0]['description']
+        #emoji = desc_emoji_dict[desc]
+        row = [dtime,humidity,temp,desc]
+        container_list.append(row)
+    
+    return(container_list)
 
+#pprint(weather_reporter(D))
