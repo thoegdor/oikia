def24ec2b174312a0de8d67df47a12bfea5f5d46
thoegdor <thoegodor@outlook.com>
2019-09-05 13:38:29 -0400

    tkinter up and running

--- gui.py
+++ gui.py
@@ -1,14 +1,23 @@
+from helper import D
+
 from PIL import Image, ImageTk
 from tkinter import Tk, Canvas, Frame, BOTH, W
 from tkinter.ttk import Frame, Label, Style
 from img import pic_chooser
+import requests
+from pprint import pprint
+import datetime
+from weather import weather_reporter
 
 class GUI(Frame):
 
     def __init__(self):
         super().__init__()
-
+        
+        self.D = D
         self.pic_chooser = pic_chooser
+        self.weather_reporter = weather_reporter
+        
         self.initUI()
 
     def initUI(self):
@@ -19,16 +28,32 @@ class GUI(Frame):
         # general
         Style().configure("TFrame", background="#333")
         
-        # text
+        ### text
         canvas = Canvas(self)
+        
+        # main title
+        canvas.create_text(
+            250,
+            10,
+            anchor=W,
+            font="Arial",
+            text=datetime.datetime.now(),
+            fill='#000000'
+        )
+           
+        # weather table   
+        weather_data = self.weather_reporter(D=self.D)
+        weather_text = '\n'.join(' '.join(map(str,sl)) for sl in weather_data)
+        
         canvas.create_text(
             200,
             300,
             anchor=W,
             font="Arial",
-            text="TEST TEXT",
+            text=weather_text,
             fill='#ff0000'
         )
+        
         canvas.pack(fill=BOTH, expand=1)
         
         # img
